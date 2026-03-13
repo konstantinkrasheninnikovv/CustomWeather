19e8bd3cae86f40c3339338049a950cd8395a6c8
Константин Крашенинников <krash7087@gmail.com>
2026-03-13 10:46:36 +0200

    Initial commit: Weather App project started

---
+++ CustomWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomWeather
+//
+//  Created by Константин Крашенинников on 13.03.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
